81cce285a78d731933f5a9804e747922d3be7d54
aohuoyun <1034482804@qq.com>
2018-03-08 09:58:23 +0800

    vue手机端框架 webpack+vue+mint-ui

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="divcss">123456</div>
+    <mt-button type="primary">primary</mt-button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang = "scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  .divcss{
+    width: 3rem;
+    height: 3rem;
+    background: blue;
+  }
+}
+</style>
